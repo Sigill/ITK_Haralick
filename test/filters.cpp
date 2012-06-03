2449429218e96b9dac06422e1ccba0bee7a2de2e
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2012-06-03 23:01:45 +0200

    Import du filtre itkScalarImageToCooccurrenceMatrixFilter.

---
+++ test/filters.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+#include <itkImageRegionIteratorWithIndex.h>
+
+#include "itkScalarImageToCooccurrenceMatrixFilter.h"
+
+const unsigned int W = 200;
+const unsigned int H = 200;
+const unsigned int D = 2;
+
+typedef itk::Image<unsigned char, D> Image2DType;
+typedef itk::ImageRegionIteratorWithIndex< Image2DType > Image2DIterator;
+typedef itk::Statistics::ScalarImageToCooccurrenceMatrixFilter< Image2DType > Image2DCoocurrenceMatrixComputer;
+
+int main(int argc, char **argv)
+{
+  Image2DType::Pointer image = Image2DType::New();
+
+  Image2DType::RegionType region;
+
+  {
+    Image2DType::IndexType index = {{ 0, 0 }};
+    Image2DType::SizeType imageSize = {{ W, H }};
+
+    region.SetIndex( index );
+    region.SetSize( imageSize );
+  }
+
+  image->SetRegions( region );
+  image->Allocate(); 
+
+  //--------------------------------------------------------------------------
+  //  1 2 1 2 1
+  //  1 2 1 2 1
+  //  1 2 1 2 1
+  //  1 2 1 2 1
+  //  1 2 1 2 1
+  //--------------------------------------------------------------------------
+  {
+    Image2DIterator imageIt( image, region );
+    Image2DType::IndexType ind;
+    for (imageIt.GoToBegin(); !imageIt.IsAtEnd(); ++imageIt)
+    {
+      ind = imageIt.GetIndex();
+      imageIt.Set(ind[1] % 2 + 1);
+    }
+  }
+
+
+  Image2DCoocurrenceMatrixComputer::Pointer coocurrenceMatrixComputer = Image2DCoocurrenceMatrixComputer::New();
+  coocurrenceMatrixComputer->SetInput(image);
+  coocurrenceMatrixComputer->SetNumberOfBinsPerAxis(4); //reasonable number of bins
+  coocurrenceMatrixComputer->SetPixelValueMinMax(0, 4);
+
+  Image2DType::OffsetType offset1 = {{0, 1}};
+  Image2DCoocurrenceMatrixComputer::OffsetVectorPointer offsetV = Image2DCoocurrenceMatrixComputer::OffsetVector::New();
+  offsetV->push_back(offset1);
+  coocurrenceMatrixComputer->SetOffsets(offsetV);
+
+  coocurrenceMatrixComputer->Update();
+
+  /*
+  for(int i = 0; i < 1000; ++i) 
+  {
+    image->Modified();
+    //std::cout << "Requested Region:" << std::endl << image->GetRequestedRegion() << std::endl;
+    //std::cout << "Requested Region:" << std::endl << image->GetRequestedRegion() << std::endl;
+  }*/
+  const Image2DCoocurrenceMatrixComputer::HistogramType* hist = coocurrenceMatrixComputer->GetOutput();
+
+  Image2DCoocurrenceMatrixComputer::HistogramType::ConstIterator histIt = hist->Begin();
+
+  while(histIt != hist->End())
+  {
+    std::cout << histIt.GetIndex() << " -> " << histIt.GetFrequency() << std::endl;
+    ++histIt;
+  }
+
+  Image2DCoocurrenceMatrixComputer::HistogramType::IndexType one_one( hist->GetMeasurementVectorSize() );
+  Image2DCoocurrenceMatrixComputer::HistogramType::IndexType one_two( hist->GetMeasurementVectorSize() );
+  Image2DCoocurrenceMatrixComputer::HistogramType::IndexType two_one( hist->GetMeasurementVectorSize() );
+  Image2DCoocurrenceMatrixComputer::HistogramType::IndexType two_two( hist->GetMeasurementVectorSize() );
+
+  one_one[0] = 1;
+  one_one[1] = 1;
+
+  one_two[0] = 1;
+  one_two[1] = 2;
+
+  two_one[0] = 2;
+  two_one[1] = 1;
+
+  two_two[0] = 2;
+  two_two[1] = 2;
+
+  float ooF, otF, toF, ttF, totalF;
+  ooF = hist->GetFrequency(one_one);
+  otF = hist->GetFrequency(one_two);
+  toF = hist->GetFrequency(two_one);
+  ttF = hist->GetFrequency(two_two);
+  totalF = hist->GetTotalFrequency();
+
+  std::cout << ooF << std::endl;
+  std::cout << otF << std::endl;
+  std::cout << toF << std::endl;
+  std::cout << ttF << std::endl;
+  std::cout << totalF << std::endl;
+
+}
