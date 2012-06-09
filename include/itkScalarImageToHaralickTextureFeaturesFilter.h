3f6fe07dcd7b46c75e4a5bf001ba16a1c3e9ce75
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2012-06-09 18:49:03 +0200

    Ajout d'un filtre composite permettant de calculer aisément les caractéristiques d'Haralick sur une région d'une image.

---
+++ include/itkScalarImageToHaralickTextureFeaturesFilter.h
@@ -0,0 +1,140 @@
+#ifndef __itkScalarImageToHaralickTextureFeaturesFilter_h
+#define __itkScalarImageToHaralickTextureFeaturesFilter_h
+
+#include "itkImageToImageFilter.h"
+#include "itkScalarImageToCooccurrenceMatrixFilter.h"
+#include "itkCooccurrenceMatrixNormalizerFilter.h"
+#include "itkCooccurrenceMatrixToHaralickTextureFeaturesFilter.h"
+
+namespace itk
+{
+namespace Statistics
+{
+
+template< class TInputImageType, class TOutputPixelType >
+class ITK_EXPORT ScalarImageToHaralickTextureFeaturesFilter:public ImageToImageFilter< TInputImageType, TInputImageType>
+{
+public:
+  /** Standard typedefs */
+  typedef ScalarImageToHaralickTextureFeaturesFilter Self;
+  typedef ProcessObject                         Superclass;
+  typedef SmartPointer< Self >                  Pointer;
+  typedef SmartPointer< const Self >            ConstPointer;
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(ScalarImageToHaralickTextureFeaturesFilter, ImageToImageFilter);
+
+  /** standard New() method support */
+  itkNewMacro(Self);
+
+  typedef TInputImageType                                   InputImageType;
+  typedef typename InputImageType::Pointer                  InputImagePointer;
+  typedef typename InputImageType::ConstPointer             InputImageConstPointer;
+  typedef typename InputImageType::PixelType                PixelType;
+  typedef typename InputImageType::RegionType               RegionType;
+  typedef typename InputImageType::SizeType                 RadiusType;
+  typedef typename InputImageType::OffsetType               OffsetType;
+
+  typedef TOutputPixelType                                  OutputPixelType;
+
+  typedef ScalarImageToCooccurrenceMatrixFilter< InputImageType >                                CooccurrenceMatrixComputer;
+  typedef typename CooccurrenceMatrixComputer::CooccurrenceMatrixType                            CooccurrenceMatrixType;
+  typedef typename CooccurrenceMatrixComputer::OffsetVector                                      OffsetVectorType;
+
+  typedef CooccurrenceMatrixNormalizerFilter< CooccurrenceMatrixType, OutputPixelType >          CooccurrenceMatrixNormalizer;
+  typedef typename CooccurrenceMatrixNormalizer::NormalizedCooccurrenceMatrixType                NormalizedCooccurrenceMatrixType;
+  typedef CooccurrenceMatrixToHaralickTextureFeaturesFilter< NormalizedCooccurrenceMatrixType >  HaralickFeaturesComputer;
+
+  ScalarImageToHaralickTextureFeaturesFilter()
+  {
+    this->SetNumberOfRequiredInputs(1);
+    this->SetNumberOfRequiredOutputs(1);
+
+    this->ProcessObject::SetNthOutput( 0, static_cast< DataObject * >( InputImageType::New().GetPointer() ) );
+
+    this->m_CooccurrenceMatrixComputer = CooccurrenceMatrixComputer::New();
+    this->m_CooccurrenceMatrixNormalizer = CooccurrenceMatrixNormalizer::New();
+    this->m_CooccurrenceMatrixNormalizer->SetInput(this->m_CooccurrenceMatrixComputer->GetOutput());
+    this->m_HaralickFeaturesComputer = HaralickFeaturesComputer::New();
+    this->m_HaralickFeaturesComputer->SetInput(this->m_CooccurrenceMatrixNormalizer->GetOutput());
+  }
+
+  void AllocateOutputs()
+  {
+    // Pass the input through as the output
+    InputImagePointer image =
+      const_cast< InputImageType * >( this->GetInput() );
+
+    this->GraftOutput(image);
+  }
+
+  using Superclass::SetInput;
+  void SetInput(const InputImageType *image)
+  {
+    this->ProcessObject::SetNthInput( 0, const_cast< InputImageType * >( image ) );
+
+    this->m_CooccurrenceMatrixComputer->SetInput(image);
+  }
+
+
+  inline typename NormalizedCooccurrenceMatrixType::MeasurementType GetFeature(typename HaralickFeaturesComputer::TextureFeatureName feature)
+  {
+    return this->m_HaralickFeaturesComputer->GetFeature(feature);
+  }
+
+
+  inline void SetOffset(const OffsetType offset)
+  {
+    this->m_CooccurrenMatrixComputer->SetOffset(offset);
+  }
+
+  inline void SetOffsets(const OffsetVectorType * offsets)
+  {
+    this->m_CooccurrenceMatrixComputer->SetOffsets(offsets);
+  }
+
+  inline const OffsetVectorType* GetOffsets()
+  {
+    return this->m_CooccurrenceMatrixComputer->GetOffset();
+  }
+
+
+  inline void SetRegionOfInterest(const RegionType roi)
+  {
+    this->m_CooccurrenceMatrixComputer->SetRegionOfInterest(roi);
+  }
+
+  inline const RegionType* GetRegionOfInterest()
+  {
+    return this->m_CooccurrenceMatrixComputer->GetRegionOfInterest();
+  }
+
+
+  inline void SetNumberOfBinsPerAxis(const unsigned char size)
+  {
+    this->m_CooccurrenceMatrixComputer->SetNumberOfBinsPerAxis(size);
+  }
+
+  inline const unsigned char GetNumberOfBinsPerAxis()
+  {
+    return this->m_CooccurrenceMatrixComputer->GetNumberOfBinsPerAxis();
+  }
+
+  
+  void GenerateData(void)
+  {
+    this->m_CooccurrenceMatrixComputer->SetInput(static_cast<const InputImageType *>(this->GetInput(0)));
+    this->m_HaralickFeaturesComputer->Update();
+  }
+
+private:
+  typename CooccurrenceMatrixComputer::Pointer     m_CooccurrenceMatrixComputer;
+  typename CooccurrenceMatrixNormalizer::Pointer   m_CooccurrenceMatrixNormalizer;
+  typename HaralickFeaturesComputer::Pointer       m_HaralickFeaturesComputer;
+
+}; 
+
+} // End of namespace Statistics
+} // End of namespace itk
+
+#endif /* __itkScalarImageToHaralickTextureFeaturesFilter_h */
---
+++ test/image_to_features.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+#include "itkScalarImageToHaralickTextureFeaturesFilter.h"
+
+#include "itkImageRegionIteratorWithIndex.h"
+
+const unsigned int W = 200;
+const unsigned int H = 200;
+const unsigned int D = 2;
+
+typedef itk::Image<unsigned char, D> ImageType;
+typedef itk::ImageRegionIteratorWithIndex< ImageType > ImageIterator;
+typedef typename itk::Statistics::ScalarImageToHaralickTextureFeaturesFilter< ImageType, double  > ScalarImageToHaralickTextureFeaturesFilter;
+
+int main(int argc, char **argv)
+{
+  ImageType::Pointer image = ImageType::New();
+
+  ImageType::RegionType region;
+
+  {
+    ImageType::IndexType index = {{ 0, 0 }};
+    ImageType::SizeType imageSize = {{ W, H }};
+
+    region.SetIndex( index );
+    region.SetSize( imageSize );
+  }
+
+  image->SetRegions( region );
+  image->Allocate();
+
+  {
+    ImageIterator imageIt( image, region );
+    ImageType::IndexType ind;
+    for (imageIt.GoToBegin(); !imageIt.IsAtEnd(); ++imageIt)
+    {
+      ind = imageIt.GetIndex();
+      imageIt.Set(ind[1] % 2 + 1);
+    }
+  }
+
+  typename ScalarImageToHaralickTextureFeaturesFilter::Pointer featuresComputer = ScalarImageToHaralickTextureFeaturesFilter::New();
+
+  featuresComputer->SetNumberOfBinsPerAxis(4);
+
+  featuresComputer->SetInput(image);
+
+  typename ScalarImageToHaralickTextureFeaturesFilter::OffsetType offset1 = {{0, 1}};
+  typename ScalarImageToHaralickTextureFeaturesFilter::OffsetVectorType::Pointer offsetV = ScalarImageToHaralickTextureFeaturesFilter::OffsetVectorType::New();
+  offsetV->push_back(offset1);
+  featuresComputer->SetOffsets(offsetV);
+
+  typename ScalarImageToHaralickTextureFeaturesFilter::InputImageType::RegionType roi;
+  typename ScalarImageToHaralickTextureFeaturesFilter::InputImageType::IndexType rIndex = {{ 0, 0 }};
+  typename ScalarImageToHaralickTextureFeaturesFilter::InputImageType::SizeType rSize = {{ 32, 32 }};
+  roi.SetSize( rSize );
+  roi.SetIndex( rIndex );
+  featuresComputer->SetRegionOfInterest(roi);
+
+  featuresComputer->Update();
+
+  std::cout << "Energy: " << featuresComputer->GetFeature(ScalarImageToHaralickTextureFeaturesFilter::HaralickFeaturesComputer::Energy) << std::endl;
+  std::cout << "Entropy: " << featuresComputer->GetFeature(ScalarImageToHaralickTextureFeaturesFilter::HaralickFeaturesComputer::Entropy) << std::endl;
+  std::cout << "Correlation: " << featuresComputer->GetFeature(ScalarImageToHaralickTextureFeaturesFilter::HaralickFeaturesComputer::Correlation) << std::endl;
+  std::cout << "InverseDifferenceMoment: " << featuresComputer->GetFeature(ScalarImageToHaralickTextureFeaturesFilter::HaralickFeaturesComputer::InverseDifferenceMoment) << std::endl;
+  std::cout << "Inertia: " << featuresComputer->GetFeature(ScalarImageToHaralickTextureFeaturesFilter::HaralickFeaturesComputer::Inertia) << std::endl;
+  std::cout << "ClusterShade: " << featuresComputer->GetFeature(ScalarImageToHaralickTextureFeaturesFilter::HaralickFeaturesComputer::ClusterShade) << std::endl;
+  std::cout << "ClusterProminence: " << featuresComputer->GetFeature(ScalarImageToHaralickTextureFeaturesFilter::HaralickFeaturesComputer::ClusterProminence) << std::endl;
+  std::cout << "HaralickCorrelation: " << featuresComputer->GetFeature(ScalarImageToHaralickTextureFeaturesFilter::HaralickFeaturesComputer::HaralickCorrelation) << std::endl;
+
+  return 0;
+}
